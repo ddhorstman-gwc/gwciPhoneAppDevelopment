21064d07efdd33d97efbd70ce1045d1efa3d9159
David Hortsman <davidhortsman@davids-macbook-pro.local>
2018-07-22 14:50:34 -0400

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by David Hortsman on 7/22/18.
+//  Copyright © 2018 David Hortsman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
